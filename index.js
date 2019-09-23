cd6367e59285d85076dee69c8b2f0b5ff9ad8840
SEAFOODS <43834030+SEAFOODS@users.noreply.github.com>
2019-09-23 07:24:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getInputVal() {
   var fname = document.getElementById("fname").value;
   var lname = document.getElementById("lname").value;
-  var name = fname + "" + lname;
+  var name = fname + " " + lname;
   
   document.getElementById("nameDisplay").innerHTML = (name);
 }
